7c4670f72ef114039484af7757ade2c73f6038de
gstory <gwj0404@163.com>
2022-08-16 10:19:29 +0800

    首次提交

---
+++ gtads/example/android/app/src/main/kotlin/com/gstory/gtads_example/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.gstory.gtads_example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ gtads/ios/Classes/GtadsPlugin.m
@@ -0,0 +1,20 @@
+#import "GtadsPlugin.h"
+
+@implementation GtadsPlugin
++ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
+  FlutterMethodChannel* channel = [FlutterMethodChannel
+      methodChannelWithName:@"gtads"
+            binaryMessenger:[registrar messenger]];
+  GtadsPlugin* instance = [[GtadsPlugin alloc] init];
+  [registrar addMethodCallDelegate:instance channel:channel];
+}
+
+- (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
+  if ([@"getPlatformVersion" isEqualToString:call.method]) {
+    result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
+  } else {
+    result(FlutterMethodNotImplemented);
+  }
+}
+
+@end
