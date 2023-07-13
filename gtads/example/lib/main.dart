16bf582c360c7d533812c4874c684eb25612e06b
gstory <gwj0404@163.com>
2023-07-13 18:09:06 +0800

    示例修改

--- gtads/example/lib/main.dart
+++ gtads/example/lib/main.dart
@@ -37,7 +37,81 @@ class _IndexState extends State<Index> {
     GTAds.addProviders(Config.providers);
     //初始化广告
     initAd = await GTAds.init(isDebug: true);
+    // loadInsertAd();
     setState(() {});
+  }
+
+  void loadInsertAd() async{
+    await GTAds.insertAd(
+        codes: Config.insertCodes,
+        isFull: false,
+        //超时时间 当广告失败后会依次重试其他广告 直至所有广告均加载失败 设置超时时间可提前取消
+        timeout: 6,
+        callBack: GTAdsCallBack(
+          onShow: (code) {
+            print("插屏广告显示 ${code.toJson()}");
+          },
+          onFail: (code, message) {
+            print("插屏广告失败 ${code?.toJson()} $message");
+          },
+          onClick: (code) {
+            print("插屏广告点击 ${code.toJson()}");
+          },
+          onClose: (code) {
+            print("插屏广告关闭 ${code.toJson()}");
+          },
+          onTimeout: () {
+            print("插屏广告加载超时");
+          },
+          onEnd: () {
+            print("插屏广告所有广告位都加载失败");
+          },
+        ));
+  }
+
+  void loadRewardAd() async{
+    await GTAds.rewardAd(
+      codes: Config.rewardCodes,
+      //奖励名称
+      rewardName: "100金币",
+      //奖励数量
+      rewardAmount: 100,
+      //用户id
+      userId: "user100",
+      //扩展参数
+      customData: "123",
+      model: GTAdsModel.RANDOM,
+      //超时时间 当广告失败后会依次重试其他广告 直至所有广告均加载失败 设置超时时间可提前取消
+      timeout: 6,
+      callBack: GTAdsCallBack(
+        onShow: (code) {
+          print("激励广告显示 ${code.toJson()}");
+        },
+        onFail: (code, message) {
+          print("激励广告失败 ${code?.toJson()} $message");
+        },
+        onClick: (code) {
+          print("激励广告点击 ${code.toJson()}");
+        },
+        onClose: (code) {
+          print("激励广告关闭 ${code.toJson()}");
+        },
+        onVerify:
+            (code, verify, transId, rewardName, rewardAmount) {
+          print(
+              "激励广告奖励验证 ${code.toJson()} $verify $transId $rewardName $rewardAmount");
+        },
+        onExpand: (code, param) {
+          print("激励广告自定义参数 ${code.toJson()} $param");
+        },
+        onTimeout: () {
+          print("激励广告加载超时");
+        },
+        onEnd: () {
+          print("激励广告所有广告位都加载失败");
+        },
+      ),
+    );
   }
 
   @override
@@ -70,53 +144,7 @@ class _IndexState extends State<Index> {
                   textColor: Colors.white,
                   child: const Text('激励广告'),
                   onPressed: () async {
-                    var b = await GTAds.rewardAd(
-                      codes: Config.rewardCodes,
-                      //奖励名称
-                      rewardName: "100金币",
-                      //奖励数量
-                      rewardAmount: 100,
-                      //用户id
-                      userId: "user100",
-                      //扩展参数
-                      customData: "123",
-                      model: GTAdsModel.RANDOM,
-                      //超时时间 当广告失败后会依次重试其他广告 直至所有广告均加载失败 设置超时时间可提前取消
-                      timeout: 6,
-                      callBack: GTAdsCallBack(
-                        onShow: (code) {
-                          print("激励广告显示 ${code.toJson()}");
-                        },
-                        onFail: (code, message) {
-                          print("激励广告失败 ${code?.toJson()} $message");
-                        },
-                        onClick: (code) {
-                          print("激励广告点击 ${code.toJson()}");
-                        },
-                        onClose: (code) {
-                          print("激励广告关闭 ${code.toJson()}");
-                        },
-                        onVerify:
-                            (code, verify, transId, rewardName, rewardAmount) {
-                          print(
-                              "激励广告奖励验证 ${code.toJson()} $verify $transId $rewardName $rewardAmount");
-                        },
-                        onExpand: (code, param) {
-                          print("激励广告自定义参数 ${code.toJson()} $param");
-                        },
-                        onTimeout: () {
-                          print("激励广告加载超时");
-                        },
-                        onEnd: () {
-                          print("激励广告所有广告位都加载失败");
-                        },
-                      ),
-                    );
-                    if (b) {
-                      print("激励广告开始请求");
-                    } else {
-                      print("激励广告开始请求失败");
-                    }
+                   loadRewardAd();
                   },
                 ),
                 MaterialButton(
@@ -125,34 +153,7 @@ class _IndexState extends State<Index> {
                   child: const Text('插屏广告'),
                   onPressed: () async {
                     print("广告位数量 ${Config.insertCodes.length}");
-                    var b = await GTAds.insertAd(
-                        codes: Config.insertCodes,
-                        isFull: false,
-                        //超时时间 当广告失败后会依次重试其他广告 直至所有广告均加载失败 设置超时时间可提前取消
-                        timeout: 6,
-                        callBack: GTAdsCallBack(
-                          onShow: (code) {
-                            print("插屏广告显示 ${code.toJson()}");
-                          },
-                          onFail: (code, message) {
-                            print("插屏广告失败 ${code?.toJson()} $message");
-                          },
-                          onClick: (code) {
-                            print("插屏广告点击 ${code.toJson()}");
-                          },
-                          onClose: (code) {
-                            print("插屏广告关闭 ${code.toJson()}");
-                          },
-                          onTimeout: () {
-                            print("插屏广告加载超时");
-                          },
-                          onEnd: () {
-                            print("插屏广告所有广告位都加载失败");
-                          },
-                        ));
-                    if (b) {
-                      print("插屏广告开始请求");
-                    }
+                    loadInsertAd();
                   },
                 ),
                 MaterialButton(
